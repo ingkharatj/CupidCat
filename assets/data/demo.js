27a023d38e1fc74ff4e4432fa6f40c6aa939c428
Thaksina <thaksina.j@ku.th>
2021-01-24 17:32:25 +0700

    Test-push

--- assets/data/demo.js
+++ assets/data/demo.js
@@ -1,115 +1,97 @@
-module.exports = [
-	{
-		id: 1,
-		name: 'Linus',
-		status: 'Online',
-		match: '78',
-		description:
-			'Good girl',
-		message:
-			'very cute',
-		image: require('../images/01.jpg')
-	},
-	{
-		id: 2,
-		name: 'Sunny',
-		match: '93',
-		description:
-			'Sunny swa',
-		status: 'Offline',
-		message: "meowww",
-		image: require('../images/02.jpg')
-	},
-	{
-		id: 3,
-		name: 'Smore',
-		match: '45',
-		description:
-			'Love owner',
-		status: 'Offline',
-		message:
-			'Like shit',
-		image: require('../images/03.jpg')
-	},
-	{
-		id: 4,
-		name: 'Ogan',
-		match: '88',
-		description:
-			'Hell cat',
-		status: 'Online',
-		message: "black and white",
-		image: require('../images/04.jpg')
-	},
-	{
-		id: 5,
-		name: 'James Dietrich',
-		match: '76',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Offline',
-		message: "It's not who I am underneath but what I do that defines me.",
-		image: require('../images/05.jpg')
-	},
-	{
-		id: 6,
-		name: 'Patricia Schulist',
-		match: '95',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
-		image: require('../images/06.jpg')
-	},
-	{
-		id: 7,
-		name: 'Chelsey Weissnat',
-		match: '67',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			"Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
-		image: require('../images/07.jpg')
-	},
-	{
-		id: 8,
-		name: 'Panda',
-		match: '85',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		age: '2',
-		location: 'Bangkok',
-		info1: 'Has eye glasses',
-		info2: 'Love milk',
-		info3: 'Like sleep',
-		info4: 'Last seen: 23h ago',
-		status: 'Online',
-		message:
-			"And as for the television's so-called plan, Batman has no jurisdiction.",
-		image: require('../images/08.jpg')
-	},
-	{
-		id: 9,
-		name: 'Glenna Reichert',
-		match: '74',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			'This is what happens when an unstoppable force meets an immovable object.',
-		image: require('../images/09.jpg')
-	},
-	{
-		id: 10,
-		name: 'Kurtis DuBuque',
-		match: '98',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Offline',
-		message:
-			'You want order in Gotham. Batman must take off his mask and turn himself in.',
-		image: require('../images/10.jpg')
-	}
+module.exports = [{
+        id: 1,
+        name: 'Linus',
+        status: 'Online',
+        match: '78',
+        description: 'Good girl',
+        message: 'very cute',
+        image: require('../images/01.jpg')
+    },
+    {
+        id: 2,
+        name: 'Sunny',
+        match: '93',
+        description: 'Sunny swa',
+        status: 'Offline',
+        message: "meowww",
+        image: require('../images/02.jpg')
+    },
+    {
+        id: 3,
+        name: 'Smore',
+        match: '45',
+        description: 'Love owner',
+        status: 'Offline',
+        message: 'Like shit',
+        image: require('../images/03.jpg')
+    },
+    {
+        id: 4,
+        name: 'Ogan',
+        match: '88',
+        description: 'Hell cat',
+        status: 'Online',
+        message: "black and white",
+        image: require('../images/04.jpg')
+    },
+    {
+        id: 5,
+        name: 'James Dietrich',
+        match: '76',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        status: 'Offline',
+        message: "It's not who I am underneath but what I do that defines me.",
+        image: require('../images/05.jpg')
+    },
+    {
+        id: 6,
+        name: 'Patricia Schulist',
+        match: '95',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        status: 'Online',
+        message: 'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
+        image: require('../images/06.jpg')
+    },
+    {
+        id: 7,
+        name: 'Chelsey Weissnat',
+        match: '67',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        status: 'Online',
+        message: "Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
+        image: require('../images/07.jpg')
+    },
+    {
+        id: 8,
+        name: 'Panda',
+        match: '85',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        age: '2',
+        location: 'Bangkok',
+        info1: 'Has eye glasses',
+        info2: 'Love milk',
+        info3: 'Like sleep',
+        info4: 'Last seen: 23h ago',
+        status: 'Online',
+        message: "And as for the television's so-called plan, Batman has no jurisdiction.",
+        image: require('../images/08.jpg')
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        match: '74',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        status: 'Online',
+        message: 'This is what happens when an unstoppable force meets an immovable object.',
+        image: require('../images/09.jpg')
+    },
+    {
+        id: 10,
+        name: 'Kurtis DuBuque',
+        match: '98',
+        description: 'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+        status: 'Offline',
+        message: 'You want order in Gotham. Batman must take off his mask and turn himself in.',
+        image: require('../images/10.jpg')
+    }
 ];
