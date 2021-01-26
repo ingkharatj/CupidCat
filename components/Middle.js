2d8cfd0f922ccce7344df9f9abd9849a77aebd0f
Thaksina <thaksina.j@ku.th>
2021-01-26 16:47:26 +0700

    push

---
+++ components/Middle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../assets/styles';
+
+import { Text, TouchableOpacity } from 'react-native';
+import Icon from './Icon';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+const Middle = () => {
+  return (
+
+      <Text style={styles.cityText}>
+        Cupid Cat
+        
+      </Text>
+
+  );
+};
+
+export default Middle;
