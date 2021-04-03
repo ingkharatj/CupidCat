ddf903a89074bc91b97006db71e65004f3f75a53
Thaksina Jansaengsri <thaksina.j@ku.th>
2021-04-03 15:31:14 +0700

    pull

--- containers/Translate.js
+++ containers/Translate.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React ,{useEffect}from 'react';
 import styles from '../assets/styles';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
+import RNSoundLevel from 'react-native-sound-level'
 
 
 import {
@@ -14,18 +16,53 @@ import ProfileItem from '../components/ProfileItem';
 import Icon from '../components/Icon';
 import Demo from '../assets/data/demo.js';
 
-const Translate = () => {
 
 
-    return (
-        <View style={styles.containerTranslate}>
-            <TouchableOpacity style={styles.bigcircledButton}>
-            <MaterialCommunityIcons name="microphone" size="60" color="#747A7A"/>
+
+class Translate extends React.Component{ 
+
+    componentDidMount =()=> {
+        RNSoundLevel.start()
+        RNSoundLevel.onNewFrame = (data) => {
+          // see "Returned data" section below
+          console.log('Sound level info', data)
+        }
+      }
+      
+      // don't forget to stop it
+      componentWillUnmount=()=> {
+        RNSoundLevel.stop()
+      }
+
+
+      render(){
+          return(
+                  <View style={styles.containerTranslate}>
+                        <TouchableOpacity style={styles.bigcircledButton}>
+                            
+                            <MaterialCommunityIcons name="microphone" size="60" color="#747A7A"/>
+                        </TouchableOpacity>
+                    </View>
+                    
+                    )
+      }
+
+
+}
+
+// const Translate = () => {
+    
+
+
+//     return (
+//         <View style={styles.containerTranslate}>
+//             <TouchableOpacity style={styles.bigcircledButton}>
+//             <MaterialCommunityIcons name="microphone" size="60" color="#747A7A"/>
 
                 
-            </TouchableOpacity>
-        </View>
-    );
-};
+//             </TouchableOpacity>
+//         </View>
+//     );
+// };
 
 export default Translate;
